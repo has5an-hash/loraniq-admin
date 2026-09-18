764f23942d96c8a32c897f1390203eaf363a0958
has5an-hash <hassan.mojtahedi.1990@gmail.com>
2026-09-19 03:19:57 +0330

    Load Loraniq Vuexy-inspired polish stylesheet globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import "./astra-reference.css";
 import "./astra-apps.css";
 import "./accents.css";
 import "./premium-final-polish.css";
+import "./loraniq-vuexy-final-polish.css";
 
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 const vazirmatn = localFont({src:"./fonts/Vazirmatn.woff2",display:"swap",variable:"--font-vazirmatn",preload:true});
