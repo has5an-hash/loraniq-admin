4af529931d399c76a923d511f00c351d3aada6a8
has5an-hash <has5an-hash@users.noreply.github.com>
2026-09-21 13:06:11 +0330

    Deploy Loraniq static site

---
+++ _next/static/chunks/2j5w6rff-ja3_.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,81135,t=>{t.v(l=>Promise.all(["static/chunks/0kd8vl9fa999w.js"].map(l=>t.l(l))).then(()=>l(13416)))}]);
